0fa59f86f019d3dd63e1f9dd3f8cefa6a3dcb8c3
lucifer1799 <vikashverma1799@gmail.com>
2021-01-12 13:36:55 +0530

    vikash verma second commit

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ function signupHandiling(){
     var name=document.getElementById("name").value;
     var email=document.getElementById("email").value;
     var pass=document.getElementById("password").value;
-    localStorage.setItem(email,pass);
+    let obj={
+        "pass" :pass,
+        "name" : name
+
+    }
+    localStorage.setItem(email,JSON.stringify(obj));
 }
 
 function loginHandiling(){
     var email=document.getElementById("email").value;
     var pass=document.getElementById("password").value;
-
-    if(pass===localStorage.getItem(email)){
+    let obj=JSON.parse(localStorage.getItem(email));
+    obj["yp"]="bro";
+    console.log(obj);
+    if(pass===obj['pass']){
         alert("access granted");
     }
     else{
